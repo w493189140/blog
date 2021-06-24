b5f934b99cc12bacbb8d39193b858abfed77726a
王成 <wangcheng55555@163.com>
2021-06-24 10:04:30 +0800

    1

--- deploy.config.js
+++ deploy.config.js
@@ -3,12 +3,12 @@ module.exports = {
     prod: {
         name: 'prod 环境', // 环境名称
         script: 'npm run build', // 打包命令
-        host: '47.98.175.99', // 服务器地址
+        host: '101.133.145.163', // 服务器地址
         port: 22, // 服务器端口号
         username: 'root', // 服务器登录用户名
         password: 'W493189140c', // 服务器登录密码 *** 代表发布时输入 为了安全尽量***
         distPath: 'blog', // 本地打包生成目录
-        webDir: '/web/webapps/blog/', // 服务器部署路径(不可为空或'/')
+        webDir: '/web/webapps/www/dist', // 服务器部署路径(不可为空或'/')
         isDelLocal: false, //上线之后是否删除本地文件dist
         projectUrl: 'https://www.jscheng.top' //项目上线后的地址
     }
